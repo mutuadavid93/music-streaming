d48ae67f675a083fe43c1fddcce13b08b982122b
David Mutua <mutuadavi@gmail.com>
2023-05-05 12:24:27 +0300

    ft: mixesinspiredby

---
+++ src/components/MixesInspiredBy.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <RouterLink :to="to" class="relative">
+      <div class="flex items-center bg-[#23232D] rounded-sm h-[54px] relative">
+        <div
+          class="flex iems-center w-[54px]"
+          @mouseenter="($event) => (isHover = true)"
+          @mouseleave="($event) => (isHover = false)"
+        >
+          <div
+            :class="
+              isHover
+                ? 'transition ease-in duration-100 bg-opacity-40'
+                : 'transition ease-out duration-100 bg-opacity-5'
+            "
+            class="absolute h-full z-10 w-[54px] bg-black rounded-sm"
+          />
+          <div
+            :class="
+              isHover
+                ? 'transition ease-in duration-100 bg-opacity-1000'
+                : 'transition ease-out duration-100 opacity-0'
+            "
+            class="absolute z-20 p-1.5 mt-1.5 rounded-full bg-white left-[6px]"
+          >
+            <Play :size="30" fillColor="#7E7E88"/>
+          </div>
+
+          <img :src="image" class="min-h-[54px] min-w-[54px] rounded-sm z-0" />
+        </div>
+        
+        <div class="truncate w-full ml-4">
+          <p
+            class="hover:underline truncate max-w-[95%] text-gray-200 font-light text-[15px] cursor-pointer"
+          >
+            {{ text }}
+          </p>
+          <p class="text-[#787882] truncate text-[12px] max-w-[95%] font-light">
+            by <span class="hover:underline cursor-pointer">{{ by }}</span>
+          </p>
+        </div>
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { toRefs, ref } from "vue";
+import { RouterLink } from "vue-router";
+import Play from 'vue-material-design-icons/Play.vue'
+
+let isHover = ref(false);
+
+const props = defineProps({ by: String, image: String, text: String, to: String });
+// Convert some props to refs
+const { by, image, text, to } = toRefs(props);
+</script>
